7cd358f70c5c82ac353f9e77e59745c78a28ef51
RobertoFavela <160526441+RobertoFavela@users.noreply.github.com>
2026-01-09 18:20:37 -0700

    Factura de Proveedor
    Reporte de Facturas de Proveedor con Monto de Anticipo

---
+++ Saldos Proveedores/Facturas.sql
@@ -0,0 +1,31 @@
+SELECT
+    OPCH."DocEntry" AS "ID Factura",
+    OPCH."DocNum" AS "No Factura",
+    OPCH."CANCELED" AS "Cancelado",
+    OPCH."DocStatus" AS "Estado del documento",
+    OPCH."DocDate" AS "Fecha de contabilizacion",
+    OPCH."CardCode" AS "Codigo de proveedor",
+    OPCH."CardName" AS "Nombre de proveedor",
+    OPCH."NumAtCard" AS "Numero de referencia de deudor",
+    OPCH."DocCur" AS "Moneda del documento",
+    PCH1."ItemCode" AS "Numero del articulo",
+    PCH1."Dscription" AS "Descripcion",
+    PCH1."LegalText" AS "Texto legal",
+    PCH1."LineTotal",
+    PCH1."TotalFrgn",
+    PCH1."AcctCode",
+    PCH1."VatSum",
+    PCH1."VatSumFrgn",
+    OPCH."PaidToDate",
+    OPCH."PaidFC",
+    DPO1."LineTotal",
+    ODPO."DocTotal",
+    ODPO."DocEntry" AS "ID Anticipo",
+    ODPO."DocNum" AS "No Anticipo",
+    OPCH."U_UDF_UUID"
+FROM OPCH
+    INNER JOIN PCH1 ON OPCH."DocEntry" = PCH1."DocEntry"
+    INNER JOIN PCH11 ON OPCH."DocEntry" = PCH11."DocEntry"
+    LEFT JOIN ODPO ON PCH11."BaseAbs" = ODPO."DocEntry"
+    LEFT JOIN DPO1 ON ODPO."DocEntry" = DPO1."DocEntry"
+ORDER BY OPCH."DocNum" DESC
