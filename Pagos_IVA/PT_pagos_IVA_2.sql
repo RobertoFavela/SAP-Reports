8cafa4415ee5009ed359081913f19b9c9f085782
RobertoFavela <160526441+RobertoFavela@users.noreply.github.com>
2026-08-04 09:44:11 -0700

    Update PT_pagos_IVA_2.sql
    Traia muchos campos incompletos, se agrego el filtro para solo traer pagos a facturas proveedor

--- Pagos_IVA/PT_pagos_IVA_2.sql
+++ Pagos_IVA/PT_pagos_IVA_2.sql
@@ -9,35 +9,40 @@ Ahora lo tomamos directamente desde OPCH para tener los datos correctos
 */
 
 SELECT
-    OJDT."RefDate" AS "Fecha",
-    OJDT."Number" AS "Diario",
-    OVPM."TrsfrDate" AS "Fecha de Pago",
-    OVPM."DocNum" AS "Folio Pago",
-    OVPM."TrsfrAcct" AS "Cuenta",
-    OACT."AcctName" AS "Banco",
-    OVPM."TrsfrSumSy" AS "Pago Total",
-    OCRD."CardName" AS "Proveedor",
-    OCRD."LicTradNum" AS "RFC",
-    OPCH."NumAtCard" AS "Folio Factura",
-    OPCH."U_UDF_UUID" AS "Folio Fiscal",
-    VPM2."AppliedSys" AS "Monto Pagado",
-    VPM2."vatAppldSy" AS "IVA",
-    VPM2."WtAppldSC" AS "Retención",
-    VPM2."WTSumSC" AS "Retención2",
-    OVPM."DocCurr" AS "Moneda",
-    OVPM."DocRate" AS "Tipo de Cambio",
-    OVPM."TrsfrSum" AS "Importe Transferido",
-    VPM2."SumApplied" AS "Pago Documento",
-    VPM2."vatApplied" AS "IVA MXP"
--- Pagos efectuados a proveedores
+    OJDT."RefDate"       AS "Fecha",
+    OJDT."Number"        AS "Diario",
+    OVPM."DocDate"       AS "Fecha de Pago",
+    OVPM."TrsfrDate"     AS "Fecha Transferencia",
+    OVPM."DocNum"        AS "Folio Pago",
+    OVPM."TrsfrAcct"     AS "Cuenta Transferencia",
+    OACT."AcctName"      AS "Banco",
+    OVPM."DocTotalSy"    AS "Pago Total SC",
+    OVPM."CardName"      AS "Proveedor",
+    OCRD."LicTradNum"    AS "RFC",
+    OPCH."NumAtCard"     AS "Folio Factura",
+    OPCH."U_UDF_UUID"    AS "Folio Fiscal",
+    VPM2."AppliedSys"    AS "Monto Pagado SC",
+    VPM2."vatAppldSy"    AS "IVA SC",
+    VPM2."WtAppldSC"     AS "Retención Aplicada SC",
+    VPM2."WTSumSC"       AS "Retención SC",
+    OVPM."DocCurr"       AS "Moneda",
+    OVPM."DocRate"       AS "Tipo de Cambio",
+    OVPM."TrsfrSum"      AS "Importe Transferido",
+    OVPM."CashSum"      AS "Importe Efectivo",
+    OVPM."CheckSum"     AS "Importe Cheque",
+    OVPM."CreditSum"     AS "Importe Credito",
+    VPM2."SumApplied"    AS "Pago Documento",
+    VPM2."vatApplied"    AS "IVA ML"
 FROM OVPM
-    -- Detalles de aplicacion de pagos (facturas relacionadas con el pago)
-    INNER JOIN VPM2 ON OVPM."DocEntry" = VPM2."DocNum"
-    -- Facturas de proovedores
-    INNER JOIN OPCH ON VPM2."DocEntry" = OPCH."DocEntry"
-    -- Datos del proveedor
-    INNER JOIN OCRD ON OPCH."CardCode" = OCRD."CardCode"
-    -- Asientos contables
-    INNER JOIN OJDT ON OVPM."TransId" = OJDT."TransId"
-    -- Catalogo de cuentas contables ("nombre del banco o cuenta")
-    LEFT JOIN OACT ON OVPM."TrsfrAcct" = OACT."AcctCode"
+INNER JOIN VPM2
+    ON OVPM."DocEntry" = VPM2."DocNum"
+   AND VPM2."InvType" = '18'
+INNER JOIN OPCH
+    ON VPM2."DocEntry" = OPCH."DocEntry"
+INNER JOIN OCRD
+    ON OVPM."CardCode" = OCRD."CardCode"
+INNER JOIN OJDT
+    ON OVPM."TransId" = OJDT."TransId"
+LEFT JOIN OACT
+    ON OVPM."TrsfrAcct" = OACT."AcctCode"
+WHERE OVPM."Canceled" = 'N';
